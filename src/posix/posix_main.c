ec87e89c529305fcc79a74a7fcce73c706043471
Sean Baggaley <sean@csnxs.uk>
2018-10-10 22:08:43 +0100

    Fix POSIX build

---
+++ src/posix/posix_main.c
@@ -0,0 +1,9 @@
+#include "../platform.h"
+
+void platform_init() {
+    /* We don't need to do anything here */
+}
+
+void platform_shutdown() {
+    /* We don't need to do anything here */
+}
--- src/posix/posix_platform.h
+++ src/posix/posix_platform.h
@@ -1,8 +1,13 @@
 #ifndef __POSIX_PLATFORM_H__
 #define __POSIX_PLATFORM_H__
 
+#include <errno.h>
+
 /* so we can use winsock and posix sockets together */
 typedef int socket_t;
 #define ioctlsocket ioctl
+
+#define SOCKERR_EAGAIN EAGAIN
+#define SOCKERR_EWOULDBLOCK EWOULDBLOCK
 
 #endif
